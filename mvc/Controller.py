3b72147ebb02b271b90cfbc64976d83d35f320e8
JonasGX <jonasgomesxavier0706@gmail.com>
2022-10-26 21:36:52 -0300

    Subindo implementacao de atividade em aula

--- mvc/Controller.py
+++ mvc/Controller.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, session, flash
-
 from Model import Model2
 
 
--- mvc/Model.py
+++ mvc/Model.py
@@ -3,24 +3,34 @@ import json
 
 cadastro = [
     {
+        "id": 50,
         'nome': "Tetris",
         'categoria': "Puzzle",
         "console": "Atari"
     },
     {
+        "id": 2,
         "nome": "God of War",
         "categoria": "Hack n Slash",
         "console": "PS2"
     },
     {
+        "id": 1000,
         "nome": "Mortal Kombat",
         "categoria": "Luta",
         "console": "PS2"
     }
 ]
+
 cadastroJSON = json.dumps(cadastro)
 
 class Model2:
     def impimeJogos():
-        return render_template('lista.html', titulo='jogos', jogos=cadastro)
-
+        lista1 = []
+        lista2 = []
+        for x in cadastro:
+            if x["id"] >= 1 and x["id"] <= 10:
+                lista1.append(x) 
+            else:
+                lista2.append(x)
+        return render_template('lista.html', titulo='jogos', jogos1=lista1, jogos2=lista2)

e6a09ea02e5b7667424802188e1d5bb150283abb
JonasGX <jonasgomesxavier0706@gmail.com>
2022-11-10 01:44:44 -0300

    Realizando interação entre o MVC com banco de dados MySQL

--- mvc/Controller.py
+++ mvc/Controller.py
@@ -1,17 +1,39 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from Model import Model2
 
-
 # Inserindo aplicação
 app = Flask(__name__)
 
-@app.route('/', methods=['GET',])
+@app.route('/',)
+def novo():
+    return Model2.novoJogo()
+
+@app.route('/jogos', methods=['GET',])
 def index():
     return Model2.impimeJogos()
 
-@app.route('/consulta', methods=['GET',])
+# ---------- Estrutura criacao
+@app.route('/criar', methods=['POST',])
+def criar():
+    return Model2.cadastrarJogo()
+
+@app.route('/bd', methods=['GET',])
+def bancodados():
+    return Model2.cadastrarJogo()
+
+# ---------- Estrutura consulta
+@app.route('/realizarconsulta', methods=['GET',])
 def consulta():
-    return Model2.consulta()
+    return Model2.consultar()
+
+@app.route('/MostrarConsulta', methods=['GET',])
+def criarconsulta():
+    return Model2.mostrarConsulta()
+
+@app.route('/criarconsulta', methods=['POST',])
+def listarconsulta():
+    return Model2.listarConsulta()
+
 
 # Fazer a aplicação rodar
 app.run(debug=True)
--- mvc/Model.py
+++ mvc/Model.py
@@ -1,41 +1,74 @@
-from urllib import response
 from flask import Flask, request, jsonify, make_response, render_template, redirect, flash
 import json
+from flask_mysqldb import MySQL
+import mysql.connector
 
-cadastro = [
-    {
-        "id": 50,
-        'nome': "Tetris",
-        'categoria': "Puzzle",
-        "console": "Atari"
-    },
-    {
-        "id": 2,
-        "nome": "God of War",
-        "categoria": "Hack n Slash",
-        "console": "PS2"
-    },
-    {
-        "id": 1000,
-        "nome": "Mortal Kombat",
-        "categoria": "Luta",
-        "console": "PS2"
-    }
-]
 
-cadastroJSON = json.dumps(cadastro)
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Acordeon2002$',
+    database='jogoteca'
+)
 
 
 class Model2:
-    def impimeJogos():
-        lista1 = []
-        lista2 = []
-        for x in cadastro:
-            if x["id"] >= 1 and x["id"] <= 10:
-                lista1.append(x)
-            else:
-                lista2.append(x)
-        return render_template('lista.html', titulo='jogos', jogos1=lista1, jogos2=lista2)
+    # Estrutura de cadastro
+    def novoJogo():
+        return render_template('novo.html', titulo='jogos')
 
-    def consulta():
-        return cadastroJSON
+    def cadastrarJogo():
+        nome = request.form['nome']
+        categoria = request.form['categoria']
+        console = request.form['console']
+
+        cursor = mydb.cursor()
+        sql = f"INSERT INTO jogos (nome, categoria, console) VALUES('{nome}','{categoria}', '{console}')"
+        cursor.execute(sql)
+        mydb.commit()
+        cursor.close()
+
+        return redirect('/jogos')
+
+    # Estrutura para mostrar os Jogos
+    def impimeJogos():
+        cursor = mydb.cursor()
+        cursor.execute('SELECT * FROM jogos')
+        todos_jogos = cursor.fetchall()
+
+        lista_jogos = []
+        for x in todos_jogos:
+            lista_jogos.append({
+                'id': x[0],
+                'nome': x[1],
+                'categoria': x[2],
+                'console': x[3]
+            })
+        return render_template('lista.html', titulo='jogos', jogo=lista_jogos )
+
+    # Estrutura de consulta
+    def consultar():
+        return render_template('consultar.html', titulo='jogos')
+    
+    def mostrarConsulta():
+        return redirect('/MostrarConsulta')
+
+    def listarConsulta():
+        id = request.form['id-consulta']
+        cursor = mydb.cursor()
+        sql = f"SELECT * FROM jogos where id = '{id}' "
+        cursor.execute(sql)
+        todos_jogos = cursor.fetchall()
+        cadastroJSON = json.dumps(todos_jogos)
+
+        jogo_consultado = []
+        for x in todos_jogos:
+            jogo_consultado.append({
+                'id': x[0],
+                'nome': x[1],
+                'categoria': x[2],
+                'console': x[3]
+            })
+        return render_template('MostrarConsulta.html', titulo='Consultar Jogos', jogo=jogo_consultado )
